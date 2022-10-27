8bc3171df14f979f86d03d4c732f7b2af7a4d0ea
Nikita.L <nlomonosov@gmail.com>
2022-10-28 00:30:35 +0300

    DZ5 completed

---
+++ DZ5/task4.py
@@ -0,0 +1,50 @@
+# Реализуйте RLE алгоритм: реализуйте модуль сжатия и восстановления данных.
+
+def rle_encode(data):
+    encoding = ''
+    prev_char = ''
+    count = 1
+
+    if not data: return ''
+
+    for char in data:
+        # If the prev and current characters
+        # don't match...
+        if char != prev_char:
+            # ...then add the count and character
+            # to our encoding
+            if prev_char:
+                encoding += str(count) + prev_char
+            count = 1
+            prev_char = char
+        else:
+            # Or increment our counter
+            # if the characters do match
+            count += 1
+    else:
+        # Finish off the encoding
+        encoding += str(count) + prev_char
+        return encoding
+
+data = str(input('Введите строку для кодирования - '))
+print (rle_encode(data))
+
+def rle_decode(data):
+    decode = ''
+    count = ''
+    for char in data:
+        # If the character is numerical...
+        if char.isdigit():
+            # ...append it to our count
+            count += char
+        else:
+            # Otherwise we've seen a non-numerical
+            # character and need to expand it for
+            # the decoding
+            decode += char * int(count)
+            count = ''
+    return decode
+
+data2 = str(input('Введите строку для декодирования - '))
+print (rle_decode(data2))
+
